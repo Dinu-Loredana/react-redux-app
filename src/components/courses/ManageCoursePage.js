3bd1fdcc32167fbb32c9e22c3f39487c39e551ef
Dinu-Loredana <dinuloredana.m@gmail.com>
2022-09-29 10:18:57 +0100

    transform ManageCoursePage class comp into funct comp

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,39 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorsActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 
-class ManageCoursePage extends React.Component {
-  componentDidMount() {
-    const { courses, authors, loadCourses, loadAuthors } = this.props;
+function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
+  useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch((error) => alert("Error fetching courses" + error));
     }
     if (authors.length === 0) {
       loadAuthors().catch((error) => alert("Error fetching authors" + error));
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <>
-        <h2>Courses</h2>
-      </>
-    );
-  }
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
 }
 
 ManageCoursePage.propTypes = {
   courses: PropTypes.array.isRequired,
   authors: PropTypes.array.isRequired,
-  loadCourses: PropTypes.function.isRequired,
-  loadAuthors: PropTypes.function.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
-    courses: state.course,
+    courses: state.courses,
     authors: state.authors,
   };
 }
