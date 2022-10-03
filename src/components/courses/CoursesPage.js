69c4df7c0a4a8eaf1a7b6faf04dca10ebf1af1d3
Dinu-Loredana <dinuloredana.m@gmail.com>
2022-10-03 14:38:22 +0100

    add Spinner to CoursesPage and ManageCoursePage

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -31,15 +31,20 @@ class CoursesPage extends React.Component {
       <>
         {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
         <h2>Courses</h2>
-        <Spinner />
-        <button
-          style={{ marginBottom: "20" }}
-          className="btn btn-primary add-course"
-          onClick={() => this.setState({ redirectToAddCoursePage: true })}
-        >
-          Add Course
-        </button>
-        <CourseList courses={this.props.courses} />
+        {this.props.loading ? (
+          <Spinner /> //show Spinner if there is at least 1 apiCallsInProgress
+        ) : (
+          <>
+            <button
+              style={{ marginBottom: "20" }}
+              className="btn btn-primary add-course"
+              onClick={() => this.setState({ redirectToAddCoursePage: true })}
+            >
+              Add Course
+            </button>
+            <CourseList courses={this.props.courses} />
+          </>
+        )}
       </>
     );
   }
@@ -49,6 +54,7 @@ CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
   authors: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 function mapStateToProps(state) {
@@ -64,6 +70,7 @@ function mapStateToProps(state) {
             };
           }),
     authors: state.authors,
+    loading: state.apiCallsInProgress > 0,
   };
 }
 
